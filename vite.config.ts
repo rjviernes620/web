52ff14fa0990f6c5674a6e45606ab2859673e23b
Roel-Junior Alejo Viernes <rjviernes620@gmail.com>
2026-06-24 03:40:32 +0100

    changed settings for vite build, proj now builds from docs

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     react(),
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        { src: 'CNAME', dest: './' }
+      ]
+    })
   ],
   resolve: {
     alias: {
